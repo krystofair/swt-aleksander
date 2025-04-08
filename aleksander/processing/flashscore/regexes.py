887f0624705155745e66403eb2a2068aeede151f
Krzysztof Kłapyta <33362868+krystofair@users.noreply.github.com>
2025-04-09 00:25:06 +0200

    another change in regexes

--- aleksander/processing/flashscore/regexes.py
+++ aleksander/processing/flashscore/regexes.py
@@ -2,6 +2,6 @@
   Module with constants to take control about regex
 """
 
-MATCH_HTML_FRAGMENT_REGEX = r"www\.flashscore\.com/match/football/([0-9a-zA-Z]+)/?\?.*$"
+MATCH_HTML_FRAGMENT_REGEX = r"www\.flashscore\.com/match/football/([0-9a-zA-Z]+)/.*$"
 DC_FRAGMENT_REGEX = r"feed/dc_1_([0-9a-zA-Z]+).*$"
 STATS_FEED_REGEX = r"feed/df_st_1_([0-9a-zA-Z]+).*$"
